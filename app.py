30d0b5f8464afb2c10d66304ccb4d1354c10ce6a
Gerard <irjecogerardo@gmail.com>
2022-08-12 20:14:35 -0600

    se creo endpoint calculadora que suma dos numeros enteros

---
+++ app.py
@@ -0,0 +1,14 @@
+from json import JSONDecoder, JSONEncoder
+import json
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/calc/<int:num1>+<int:num2>', methods=['POST'])
+def calc(num1, num2):
+    return jsonify({'result': num1 + num2})
